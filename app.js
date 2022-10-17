f28d41a14044ca81d1e893c351ece667d6d27e34
Julian Timbs <juliantimbs1@gmail.com>
2022-10-17 21:40:27 +1000

    Added background-colour change on hover

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const container = document.getElementById("container");
 
+
+
 function createGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
 
-const grid = createGrid(16, 16);
+const grid = createGrid(12, 12);
+const gridItem = Array.from(document.querySelectorAll('.grid-item'));
 
-square = document.addEventListener('hover', active);
 
-function active() {
-    const square = document.querySelector('grid-item');
-    square.classList.add('active');
-}
+gridItem.forEach(gridItem => gridItem.addEventListener('mouseover', () => {
+  gridItem.classList.add('active');
+}));
+
+const btn = document.querySelector('.size')
+
+
+    
+
